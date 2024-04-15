33599e63d7cd4f8b8ce2b8f33a7a0b9bc6a7db75
Betim Beja <11160171+BetimBeja@users.noreply.github.com>
2024-04-15 22:00:31 +0200

    fix: Improved ManifestParser

---
+++ __tests__/ManifestParser.test.ts
@@ -0,0 +1,129 @@
+import { describe, expect, test } from "@jest/globals";
+import { ManifestParser } from "../src/ManifestParser";
+import fs from "fs/promises";
+
+describe("ManifestParser", () => {
+  test("can parse Manifest", async () => {
+    const manifest = await fs.readFile(
+      require.resolve("./Calendar/ControlManifest.Input.xml")
+    );
+    var parser = new ManifestParser(true);
+    expect(await parser.Parse(manifest)).toEqual({
+      Component: "Calendar",
+      ComponentIndex: "index.ts",
+      MockGenerator: "ComponentFrameworkMockGeneratorReact",
+      Inputs: [
+        {
+          type: "Multiple",
+          name: "Theme",
+          usage: "input",
+        },
+        {
+          type: "SingleLine.Text",
+          name: "BackgroundColor",
+          usage: "input",
+        },
+        {
+          type: "SingleLine.Text",
+          name: "Language",
+          usage: "input",
+        },
+        {
+          type: "SingleLine.Text",
+          name: "AccessibilityLabel",
+          usage: "input",
+        },
+        {
+          type: "DateAndTime.DateOnly",
+          name: "SelectedDateValue",
+          usage: "bound",
+        },
+        {
+          type: "DateAndTime.DateOnly",
+          name: "MinDate",
+          usage: "input",
+        },
+        {
+          type: "DateAndTime.DateOnly",
+          name: "MaxDate",
+          usage: "input",
+        },
+        {
+          type: "TwoOptions",
+          name: "ShowGoToToday",
+          usage: "input",
+        },
+        {
+          type: "TwoOptions",
+          name: "MonthPickerVisible",
+          usage: "input",
+        },
+        {
+          type: "TwoOptions",
+          name: "DayPickerVisible",
+          usage: "input",
+        },
+        {
+          type: "TwoOptions",
+          name: "HighlightSelectedMonth",
+          usage: "input",
+        },
+        {
+          type: "TwoOptions",
+          name: "HighlightCurrentMonth",
+          usage: "input",
+        },
+        {
+          type: "TwoOptions",
+          name: "ShowSixWeeksByDefault",
+          usage: "input",
+        },
+        {
+          type: "TwoOptions",
+          name: "ShowWeekNumbers",
+          usage: "input",
+        },
+        {
+          type: "SingleLine.Text",
+          name: "InputEvent",
+          usage: "input",
+        },
+        {
+          type: "Enum",
+          name: "FirstDayOfWeek",
+          usage: "input",
+          enumValues: [
+            {
+              name: "Sunday",
+              value: "Sunday",
+            },
+            {
+              name: "Monday",
+              value: "Monday",
+            },
+            {
+              name: "Tuesday",
+              value: "Tuesday",
+            },
+            {
+              name: "Wednesday",
+              value: "Wednesday",
+            },
+            {
+              name: "Thursday",
+              value: "Thursday",
+            },
+            {
+              name: "Friday",
+              value: "Friday",
+            },
+            {
+              name: "Saturday",
+              value: "Saturday",
+            },
+          ],
+        },
+      ],
+    });
+  });
+});
--- src/ManifestParser.ts
+++ src/ManifestParser.ts
@@ -1,14 +1,56 @@
 import libxmljs, { XMLElement } from "libxmljs";
-import { info } from './tracing';
+import { info } from "./tracing";
+import { IData, IProperty } from "ShkoOnline.types";
 
 export class ManifestParser {
-    private VERBOSE: boolean;
-    constructor(VERBOSE: boolean) {
-        this.VERBOSE = VERBOSE;
-    }
-    async Parse(manifest: Buffer) {
-        const xmlDoc = await libxmljs.parseXmlAsync(manifest);
-        const control = xmlDoc.find("/manifest/control");
-        info((control[0] as XMLElement).getAttribute("constructor")?.value() || "");
-    }
+  private VERBOSE: boolean;
+  constructor(VERBOSE: boolean) {
+    this.VERBOSE = VERBOSE;
+  }
+  async Parse(manifest: Buffer) {
+    const xmlDoc = await libxmljs.parseXmlAsync(manifest);
+    const control = xmlDoc.find("/manifest/control")[0] as XMLElement;
+    const result = {
+      Component: control.getAttribute("constructor")?.value(),
+      ComponentIndex: (
+        xmlDoc.find(
+          "/manifest/control/resources/code[@order='1']"
+        )[0] as XMLElement
+      )
+        .getAttribute("path")
+        ?.value() as string,
+      MockGenerator:
+        control.getAttribute("control-type")?.value() === "virtual"
+          ? "ComponentFrameworkMockGeneratorReact"
+          : "ComponentFrameworkMockGenerator",
+      Inputs: xmlDoc.find("/manifest/control/property").map((p) => {
+        const type = (p as XMLElement)
+          .getAttribute("of-type")
+          ?.value() as IProperty["type"];
+        const name = (p as XMLElement).getAttribute("name")?.value() as string;
+        const usage = (p as XMLElement)
+          .getAttribute("usage")
+          ?.value() as IProperty["usage"];
+        if (type === "Enum") {
+          const enumValues = p.find("value").map((v) => ({
+            name: (v as XMLElement).getAttribute("name")?.value() as string,
+            value: (v as XMLElement).text(),
+          }));
+          return {
+            type,
+            name,
+            usage,
+            enumValues,
+          };
+        }
+        return {
+          type,
+          name,
+          usage,
+        };
+      }),
+    } as IData;
+
+    return result;
+  }
 }
