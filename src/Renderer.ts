186de2bd1aec5e2b7fca25d9133eff855c37b9fb
asllanmakaj7 <animakaj7@gmail.com>
2024-04-15 12:28:26 +0200

    fix: added OutputProperties on template

--- src/Renderer.ts
+++ src/Renderer.ts
@@ -1,70 +1,130 @@
-import { IData } from 'ShkoOnline.types';
-import { readFile } from 'fs/promises';
-import Handlebars from 'handlebars';
+import { IData } from "ShkoOnline.types";
+import { readFile } from "fs/promises";
+import Handlebars from "handlebars";
 
 export default class Renderer {
-    constructor() {
-        Handlebars.registerHelper('GetPropertyMocks', this.GetPropertyMocks.bind(this));
-        Handlebars.registerHelper('ControlToPropertyMock', this.ControlToPropertyMock.bind(this));
-        Handlebars.registerHelper('ControlToType', this.ControlToType.bind(this));
-        Handlebars.registerHelper('isVirtualComponent', this.isVirtualComponent.bind(this));
+  constructor() {
+    Handlebars.registerHelper(
+      "GetPropertyMocks",
+      this.GetPropertyMocks.bind(this)
+    );
+    Handlebars.registerHelper(
+      "ControlToPropertyMock",
+      this.ControlToPropertyMock.bind(this)
+    );
+    Handlebars.registerHelper("ControlToType", this.ControlToType.bind(this));
+    Handlebars.registerHelper(
+      "isVirtualComponent",
+      this.isVirtualComponent.bind(this)
+    );
+    Handlebars.registerHelper(
+      "GetOutputProperties",
+      this.GetOutputProperties.bind(this)
+    );
+  }
+
+  private GetPropertyMocks(inputs: IData["Inputs"]) {
+    const propertyMocks: string[] = [];
+    inputs.forEach((property) => {
+      var mock = this.ControlToPropertyMock(property);
+      if (!propertyMocks.includes(mock)) {
+        propertyMocks.push(mock);
+      }
+    });
+    return propertyMocks.join(", ");
+  }
+
+  private isVirtualComponent(mockGenerator: IData["MockGenerator"]) {
+    return mockGenerator === "ComponentFrameworkMockGeneratorReact";
+  }
+
+  private ControlToPropertyMock(control: IData["Inputs"][any]) {
+    switch (control.type) {
+      case "DateAndTime.DateOnly":
+        return "DateTimePropertyMock";
+      case "Multiple":
+        return "StringPropertyMock";
+      case "SingleLine.Text":
+        return "StringPropertyMock";
+      case "TwoOptions":
+        return "TwoOptionsPropertyMock";
+      case "Enum":
+        return "EnumPropertyMock";
+      default:
+        return "ERROR!" + control;
     }
+  }
 
-    private GetPropertyMocks(inputs: IData['Inputs']) {
-        const propertyMocks: string[] = [];
-        inputs.forEach(property => {
-            var mock = this.ControlToPropertyMock(property);
-            if (!propertyMocks.includes(mock)) {
-                propertyMocks.push(mock);
-            }
-        });
-        return propertyMocks.join(', ');
+  private ControlToType(control: IData["Inputs"][any]) {
+    switch (control.type) {
+      case "DateAndTime.DateOnly":
+        return "Date";
+      case "Multiple":
+      case "SingleLine.Text":
+        return "string";
+      case "TwoOptions":
+        return "boolean";
+      case "Enum":
+        return control.enumValues.map((v) => v.value).join(" | ");
+      default:
+        return "";
     }
-
-    private isVirtualComponent(mockGenerator: IData['MockGenerator']) {
-        return mockGenerator === 'ComponentFrameworkMockGeneratorReact';
+  }
+  private ControlToOutputTypes(control: IData["Inputs"][any]) {
+    switch (control.type) {
+      case "DateAndTime.DateAndTime":
+      case "DateAndTime.DateOnly":
+        return "Date";
+      case "Currency":
+      case "Decimal":
+      case "Whole.None":
+      case "FP":
+      case "OptionSet":
+        return "number";
+      case "Lookup.Simple":
+        return "Lookup";
+      case "Multiple":
+        return "string";
+      case "Object":
+        return "any";
+      case "MultiSelectOptionSet":
+        return "number[]";
+      case "SingleLine.Email":
+      case "SingleLine.Phone":
+      case "SingleLine.Text":
+      case "SingleLine.TextArea":
+      case "SingleLine.Ticker":
+      case "SingleLine.URL":
+        return "string";
+      case "TwoOptions":
+        return "boolean";
+      default:
+        return "ERROR!" + control;
     }
+  }
 
-    private ControlToPropertyMock(control: IData['Inputs'][any]) {
-        switch (control.type) {
-            case 'DateAndTime.DateOnly':
-                return 'DateTimePropertyMock';
-            case 'Multiple':
-                return 'StringPropertyMock';
-            case 'SingleLine.Text':
-                return 'StringPropertyMock';
-            case 'TwoOptions':
-                return 'TwoOptionsPropertyMock';
-            case 'Enum':
-                return 'EnumPropertyMock';
-            default:
-                return 'ERROR!'+control;
-        }
-    }
+  async Render(data: IData) {
+    const buffer = await readFile(
+      require.resolve("./renderGenerator.template.hbs")
+    );
+    const template = Handlebars.compile(buffer.toString());
 
-    private ControlToType(control: IData['Inputs'][any]) {
-        switch (control.type) {
-            case 'DateAndTime.DateOnly':
-                return 'Date';
-            case 'Multiple':
-            case 'SingleLine.Text':
-                return 'string';
-            case 'TwoOptions':
-                return 'boolean';
-            case 'Enum':
-                return control.enumValues.map(v=>v.value).join(" | ");
-            default:
-                return '';
-        }
-    }
+    const dts = template({ data });
 
-    async Render(data: IData) {
-        const buffer = await readFile(require.resolve('./renderGenerator.template.hbs'));
-        const template = Handlebars.compile(buffer.toString());
+    return dts;
+  }
 
-
-        const dts = template({ data });
-
-        return dts;
-    }
+  private GetOutputProperties(inputs: IData["Inputs"]) {
+    var t = inputs
+      .filter(
+        (property) => property.usage == "bound" || property.usage == "output"
+      )
+      .map((property) => {
+        return {
+          name: property.name,
+          type: this.ControlToOutputTypes(property),
+        };
+      });
+    return t;
+  }
 }
--- src/ShkoOnline.types.d.ts
+++ src/ShkoOnline.types.d.ts
@@ -1,40 +1,63 @@
 export type IEnumValue = {
-    name: string;
-    value: string;
-}
+  name: string;
+  value: string;
+};
 
 export type IProperty = {
-    defaultValue?: string;
-    name: string;
-    required?: boolean;
-    usage: 'input' | 'bound' | 'output'
-} & ({
-    type: 'DateAndTime.DateOnly' | 'Multiple' | 'SingleLine.Text' | 'TwoOptions'
-} | {
-    type: 'Enum',
-    enumValues: IEnumValue[]
-})
+  defaultValue?: string;
+  name: string;
+  required?: boolean;
+  usage: "input" | "bound" | "output";
+} & (
+  | {
+      type:
+        | "Currency"
+        | "DateAndTime.DateAndTime"
+        | "DateAndTime.DateOnly"
+        | "Decimal"
+        | "FP"
+        | "Lookup.Simple"
+        | "MultiSelectOptionSet"
+        | "Multiple"
+        | "Object"
+        | "OptionSet"
+        | "SingleLine.Email"
+        | "SingleLine.Phone"
+        | "SingleLine.Text"
+        | "SingleLine.TextArea"
+        | "SingleLine.Ticker"
+        | "SingleLine.URL"
+        | "TwoOptions"
+        | "Whole.None";
+    }
+  | {
+      type: "Enum";
+      enumValues: IEnumValue[];
+    }
+);
 
 export type IData = {
-    /**
-     * Class Name of the component
-     */
-    Component: string;
-    /**
-     * Component folder relative to the renderGenerator
-     */
-    ComponentFolder: string;
-    /**
-     * Component index file relative to the ComponentFolder. Defined in ControlManifest.Input.xml in the resources node like: `<code path="index.ts" order="1" />`
-     */
-    ComponentIndex: string;
-    /**
-     * List of inputs
-     */
-    Inputs: IProperty[];
-    /**
-     * Mock Generator type (Starndard | React)
-     */
-    MockGenerator: 'ComponentFrameworkMockGenerator' | 'ComponentFrameworkMockGeneratorReact'
+  /**
+   * Class Name of the component
+   */
+  Component: string;
+  /**
+   * Component folder relative to the renderGenerator
+   */
+  ComponentFolder: string;
+  /**
+   * Component index file relative to the ComponentFolder. Defined in ControlManifest.Input.xml in the resources node like: `<code path="index.ts" order="1" />`
+   */
+  ComponentIndex: string;
+  /**
+   * List of inputs
+   */
+  Inputs: IProperty[];
 
-}
+  /**
+   * Mock Generator type (Starndard | React)
+   */
+  MockGenerator:
+    | "ComponentFrameworkMockGenerator"
+    | "ComponentFrameworkMockGeneratorReact";
+};
